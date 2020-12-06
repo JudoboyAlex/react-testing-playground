d30942ac6af85dbc5f4418bdc6091d0dafb63865
Alexander G. Yang <spark8402@rogers.com>
2020-12-06 18:07:16 -0500

    added react testing library

--- src/tests/WorkOrdersDisplay.test.js
+++ src/tests/WorkOrdersDisplay.test.js
@@ -1,14 +1,28 @@
 
 import * as React from 'react'
 import ReactDOM from 'react-dom'
+import {render, screen, fireEvent} from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
 import WorkOrdersDisplay from '../components/WorkOrdersDisplay'
 
-test('renders WorkOrdersDisplay component', () => {
-  const div = document.createElement('div')
-  document.body.append(div)
-  ReactDOM.render(<WorkOrdersDisplay />, div)
-  console.log(document.body.innerHTML);
+/* 
+Testing React Using Jest
+// test('renders WorkOrdersDisplay component', () => {
+//   const div = document.createElement('div')
+//   document.body.append(div)
+//   ReactDOM.render(<WorkOrdersDisplay />, div)
+//   console.log(document.body.innerHTML);
+//   const message = div.querySelector('ul')
+//   console.log(message);
+// })
+*/
 
+// Testing React Using React Testing Library
+test('renders WorkOrdersDisplay component with RTL', () => {
+  render(<WorkOrdersDisplay/>)
+  // const view = container.querySelector(".workOrderDetails")
+  const view = screen.queryAllByText('listitem')
+  console.log(view)
 })
 
 
